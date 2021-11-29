c398fa60814a79ba651e6c79be699cd8a4c96eb1
Wishforpeace <90396662+Wishforpeace@users.noreply.github.com>
2021-11-29 22:18:49 +0800

    Update main.go

--- checkpoint5/main.go
+++ checkpoint5/main.go
@@ -10,19 +10,19 @@ import (
 func main() {
 	client := &http.Client{}
 	passport := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiMTIwIiwiaWF0IjoxNjM3MTQ2NDM1LCJuYmYiOjE2MzcxNDY0MzV9.mlggHuQMg4eooV1KBB9scFQE-J7018S5RLpXl-boWX4"
-	method ：= "GET"
-	url：="http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/muxi/backend/computer/examination"
-	request,_ ：= http.NewRequest(method,url,nil)
+	method := "GET"
+	url :="http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/muxi/backend/computer/examination"
+	request,_ := http.NewRequest(method,url,nil)
 	request.Header.Add("Code","250")
 	request.Header.Add("Passport",passport)
-	response,_ ：= client.Do(request)
-	body ,_ ：= ioutil.ReadAll(response.Body)
+	response,_ := client.Do(request)
+	body ,_ := ioutil.ReadAll(response.Body)
 	fmt.Println(string(body))
 	//最后一关
 	//find the correct token
-	bodyBuf ：= &bytes.Buffer{}
-	bodyWriter ：= multipart.NewWriter(bodyBuf)
-	writer, _ ：= bodyWriter.CreateFormFile("file", "permute.go")
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+	writer, _ := bodyWriter.CreateFormFile("file", "permute.go")
 	file, _ := os.Open("./permute.go")
 	io.Copy(writer, file)
 	bodyWriter.Close()
