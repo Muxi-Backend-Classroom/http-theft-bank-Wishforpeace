86aca219c7616a5c681b8cc5e6427389b6260279
Wishforpeace <90396662+Wishforpeace@users.noreply.github.com>
2021-11-29 21:38:22 +0800

    Update main.go

--- checkpoint1/main.go
+++ checkpoint1/main.go
@@ -1,14 +1,34 @@
 package main
-import(
-"fmt"
-"net/http"
-"io/ioutil"
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
 )
-func main(){
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code", nil)
-	req.Header.Add("Code", "120")
-	res, _ := client.Do(req)
-	by, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(by))
+
+func main() {
+
+	url := "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code"
+	method := "GET"
+
+  	client := &http.Client {
+  }
+  	request,err := http.NewRequest(method, url, nil)
+	if err != nil {
+		fmt.Println(err)
+    	return
+  	}
+  	request.Header.Add("Code","250")//添加code
+  	response, err := client.Do(request)
+  	if err != nil {
+    	fmt.Println(err)
+    	return
+  	}
+	fmt.Printf("Header1\n")
+	fmt.Println(response.Header)
+	body,err := ioutil.ReadAll(response.Body)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
